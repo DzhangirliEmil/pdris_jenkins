eb7ee7f317f02c2a574f75e353d0f0bed389951d
DzhangirliEmil <79907936+DzhangirliEmil@users.noreply.github.com>
2022-12-17 23:08:24 +0300

    Update Main.java

--- src/main/java/sbt/pdris/Main.java
+++ src/main/java/sbt/pdris/Main.java
@@ -2,7 +2,7 @@ package sbt.pdris;
 
 public class Main {
     public static void main(String[] args) {
-        //try? pls work plspls plspls
+        //try? pls work plspls plspls plspls
         System.out.println("Hello world!");
     }
 }
